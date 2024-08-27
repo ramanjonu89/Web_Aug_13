8f784e1591757ee97de4a0d24e23644bb3647cf5
ramanjonu89 <70876972+ramanjonu89@users.noreply.github.com>
2024-08-27 16:15:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BlogProvider } from "./Context/BlogContext.jsx";
+import { BlogProvider } from "./BlogContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
