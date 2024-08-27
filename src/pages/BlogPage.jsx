7b5f351a2e618f87f521619122b89530494acca2
ramanjonu89 <70876972+ramanjonu89@users.noreply.github.com>
2024-08-27 16:06:10 +0530

    Update BlogPage.jsx

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState, useContext } from "react";
 import BLOGBAR from "../assets/Rectangle 1084BLOGBAR.svg";
 import BLOG1 from "../assets/Rectangle 1070BLOG1.svg";
 import BLOG2 from "../assets/Rectangle 1070BLOG2.svg";
@@ -6,9 +6,17 @@ import BLOG3 from "../assets/Rectangle 1070BLOG3.svg";
 import BLOG4 from "../assets/Rectangle 1070BLOG4.svg";
 import BLOG5 from "../assets/Rectangle 1070BLOG5.svg";
 import BLOG6 from "../assets/Rectangle 1070BLOG6.svg";
+
 import "./BlogPageCSS.css";
+import PortfolioPage from "./PortfolioPage";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { BlogContext } from "../Context/BlogContext";
 
 const BlogPage = () => {
+  const {  setblogId,setIsblogpage } = useContext(BlogContext);
+
+  const navigate = useNavigate();
   const blogPageRef = useRef(null);
   const cardRefs = useRef([]);
 
@@ -49,144 +57,75 @@ const BlogPage = () => {
     };
   }, []);
 
+  const [blogs, setBlogs] = useState([]);  //state to set data from google sheet
+
+
+  // fecthed data from google sheet 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://sheetdb.io/api/v1/rsix5ycm9i42k`
+        );   // use .env file for this api endpoint
+
+        const rows = response.data;
+        setBlogs(rows);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+
+  // function runs when blog title clciked 
+  const handleBlog = (id) => {
+    setIsblogpage(true); // show blogs internal page
+    setblogId(id);     // setting id of page which is to be shown
+    const targetSection = document.getElementById('bloginternal');   // getting id of internal blogs page
+
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });  //smoothly navigating to respective page
+    }
+  };
+
   return (
     <div className="blogpage-container" id="blog" ref={blogPageRef}>
       <div className="blogpage-heading">
         <h1 className="heading-left">BLOG</h1>
         <h1 className="stroke heading-right">YARD</h1>
       </div>
-      
-      <div
-        className="blogpage-blog-one-container card"
-        ref={(el) => (cardRefs.current[0] = el)}
-      >
-        <div className="blogpage-blog-image-container">
-          <img src={BLOG1} alt="blog-one" />
-        </div>
-        <div className="blogpage-blog-text-container">
-          <div className="blog-card-title">
-            <h1 className="stroke">01 </h1>
-            <h1>Blog Title</h1>
-          </div>
-          <h6>DECEMBER 27, 2022</h6>
-          <img src={BLOGBAR} alt="barimg" />
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
-      </div>
 
-      <div
-        className="blogpage-blog-two-container card"
-        ref={(el) => (cardRefs.current[1] = el)}
-      >
-        <div className="blogpage-blog-text-container">
-          <div className="blog-card-title">
-            <h1 className="stroke">02 </h1>
-            <h1>Blog Title</h1>
-          </div>
-          <h6>DECEMBER 27, 2022</h6>
-          <img src={BLOGBAR} alt="barimg" />
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
-        <div className="blogpage-blog-image-container">
-          <img src={BLOG2} alt="blog-two" />
-        </div>
-      </div>
+      {/* mapping over the data i.e coming from google sheet  */}
+      {blogs.map((blog, index) => {
+        // Determine image based on blog index or any logic you prefer
+        const blogImage = [BLOG1, BLOG2, BLOG3, BLOG4, BLOG5, BLOG6][index % 6];
 
-      <div
-        className="blogpage-blog-one-container card"
-        ref={(el) => (cardRefs.current[2] = el)}
-      >
-        <div className="blogpage-blog-image-container">
-          <img src={BLOG3} alt="blog-one" />
-        </div>
-        <div className="blogpage-blog-text-container">
-          <div className="blog-card-title">
-            <h1 className="stroke">03 </h1>
-            <h1>Blog Title</h1>
+        return (
+          <div
+            className={`blogpage-blog-${index % 2 === 0 ? 'one' : 'two'}-container card in-view`}
+            ref={(el) => (cardRefs.current[index] = el)}
+            key={blog.id} // Unique key for each card
+          >
+            <div className="blogpage-blog-image-container">
+              <img src={blogImage} alt={`blog-${index}`} />
+            </div>
+            <div className="blogpage-blog-text-container">
+              <div className="blog-card-title">
+                <h1 className="stroke">{String(index + 1).padStart(2, '0')}</h1>
+                <div onClick={() => handleBlog(blog.id)}>
+                  <h1 style={{ cursor:"pointer"}}>{blog.Title}</h1>
+                </div>
+              </div>
+              <h6>DECEMBER 27, 2022</h6>
+              <img src={BLOGBAR} alt="barimg" />
+              <p>{blog.Description}</p>
+            </div>
           </div>
-          <h6>DECEMBER 27, 2022</h6>
-          <img src={BLOGBAR} alt="barimg" />
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
-      </div>
-
-      <div
-        className="blogpage-blog-two-container card"
-        ref={(el) => (cardRefs.current[3] = el)}
-      >
-        <div className="blogpage-blog-text-container">
-          <div className="blog-card-title">
-            <h1 className="stroke">04 </h1>
-            <h1>Blog Title</h1>
-          </div>
-          <h6>DECEMBER 27, 2022</h6>
-          <img src={BLOGBAR} alt="barimg" />
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
-        <div className="blogpage-blog-image-container">
-          <img src={BLOG4} alt="blog-two" />
-        </div>
-      </div>
-
-      <div
-        className="blogpage-blog-one-container card"
-        ref={(el) => (cardRefs.current[4] = el)}
-      >
-        <div className="blogpage-blog-image-container">
-          <img src={BLOG5} alt="blog-one" />
-        </div>
-        <div className="blogpage-blog-text-container">
-          <div className="blog-card-title">
-            <h1 className="stroke">05 </h1>
-            <h1>Blog Title</h1>
-          </div>
-          <h6>DECEMBER 27, 2022</h6>
-          <img src={BLOGBAR} alt="barimg" />
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
-      </div>
-
-      <div
-        className="blogpage-blog-two-container card"
-        ref={(el) => (cardRefs.current[5] = el)}
-      >
-        <div className="blogpage-blog-text-container">
-          <div className="blog-card-title">
-            <h1 className="stroke">06 </h1>
-            <h1>Blog Title</h1>
-          </div>
-          <h6>DECEMBER 27, 2022</h6>
-          <img src={BLOGBAR} alt="barimg" />
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
-        <div className="blogpage-blog-image-container">
-          <img src={BLOG6} alt="blog-two" />
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
